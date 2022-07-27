c552aa83bd656f931a9fb3ca730996cde2f17450
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2022-07-27 15:23:06 -0400

    Added entry point for ckan plugin.

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     # have to change the message extractors, you can read more about babel
     # message extraction at
     # http://babel.pocoo.org/docs/messages/#extraction-method-mapping-and-configuration
+    entry_points='''
+        [ckan.plugins]
+        gcnotify=ckanext.gcnotify.plugin:GcnotifyPlugin
+    ''',
     message_extractors={
         'ckanext': [
             ('**.py', 'python', None),
