5aaf2b92ed803eb24ac40438f1fe09489c6bd42c
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2022-07-28 10:35:33 -0400

    Added logic for attachments.

--- ckanext/gcnotify/plugin.py
+++ ckanext/gcnotify/plugin.py
@@ -5,11 +5,12 @@ import ckan.model as model
 
 import ckan.lib.mailer as mailer
 
-from ckan.lib.mailer import MailerException, Attachment
-from typing import Any, Iterable, Optional
+import base64
+from typing import Any, Iterable, Optional, cast
 from ckan.common import _, config
 
 from notifications_python_client.notifications import NotificationsAPIClient
+from notifications_python_client.utils import DOCUMENT_UPLOAD_SIZE_LIMIT
 
 
 class GcnotifyPlugin(plugins.SingletonPlugin):
@@ -30,39 +31,56 @@ class GcnotifyPlugin(plugins.SingletonPlugin):
               body: str,
               body_html: Optional[str] = None,
               headers: Optional[dict[str, Any]] = None,
-              attachments: Optional[Iterable[Attachment]] = None) -> None:
+              attachments: Optional[Iterable[mailer.Attachment]] = None) -> None:
 
         if (recipient.email is None) or not len(recipient.email):
-            raise MailerException(_("No recipient email address available!"))
+            raise mailer.MailerException(_("No recipient email address available!"))
 
         if (self.apiKey is None) or not len(self.apiKey):
-            raise MailerException(_("No GC Notify API key is set!"))
+            raise mailer.MailerException(_("No GC Notify API key is set!"))
 
         if (self.baseURI is None) or not len(self.baseURI):
-            raise MailerException(_("No GC Notify base URI is set!"))
+            raise mailer.MailerException(_("No GC Notify base URI is set!"))
 
         if (self.templateID is None) or not len(self.templateID):
-            raise MailerException(_("No GC Notify template ID is set!"))
+            raise mailer.MailerException(_("No GC Notify template ID is set!"))
 
         if body_html is not None:
             body = body_html
 
         if (body is None) or not len(body):
-            raise MailerException(_("No email body is set!"))
+            raise mailer.MailerException(_("No email body is set!"))
 
         notificactions_client = NotificationsAPIClient(self.apiKey)
+        personalisationData = {
+            'subject': subject,
+            'rendered_body': body,
+        }
 
         if attachments is not None:
-            #TODO: hanled attachments.
-            attachments = attachments
+            for index, attachment in attachments:
+                if len(attachment) == 3:
+                    name, _file, media_type = cast(mailer.AttachmentWithType, attachment)
+                else:
+                    name, _file = cast(mailer.AttachmentWithoutType, attachment)
+                    media_type = None
+
+                contents = _file.read()
+                sendingMethod = 'attach'
+
+                if len(contents) > DOCUMENT_UPLOAD_SIZE_LIMIT:
+                    sendingMethod = 'link'
+
+                personalisationData[f"file_{index}"] = {
+                    'file': base64.b64encode(contents).decode('ascii'),
+                    'filename': name,
+                    'sending_method': sendingMethod
+                }
 
         notificactions_client.send_email_notification(
             email_address=recipient.email,
             template_id=self.templateID,
-            personalisation={
-                'subject': subject,
-                'rendered_body': body,
-            }
+            personalisation=personalisationData
         )
 
     
