0f21570bf8479ed310ee3387fc86b84ce40e3ed8
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2022-08-03 16:58:25 -0400

    Finalized functional type code. Removed try-catches and added asserts for config keys.

---
+++ ckanext/gcnotify/mailer.py
@@ -0,0 +1,188 @@
+from ckan.lib.helpers import ckan_version
+from ckan.lib.helpers import roles_translated
+
+from ckan.common import _, config
+import ckan.lib.mailer as mailer
+
+import requests
+import json
+
+
+def get_template_id(action):
+    # type: (str) -> str|None
+
+    template_ids = config.get('ckanext.gcnotify.template_ids') # type: dict|None
+    if not template_ids:
+      template_ids = {}
+    else:
+      template_ids = json.loads(template_ids)
+
+    if action not in template_ids:
+      raise mailer.MailerException(_("No GC Notify template ID is set!"))
+
+    return template_ids.get(action)
+
+
+def send_reset_link(user):
+    # type: (ckan.model.User) -> None
+
+    if not user.email:
+      raise mailer.MailerException(_("No recipient email address available!"))
+
+    # use user ID, use user fullname if it is set
+    user_name = user.name
+    if user.fullname:
+      user_name = user.fullname
+
+    # generate a user reset key, then get it
+    mailer.create_reset_key(user)
+    reset_link = mailer.get_reset_link(user)
+    
+    send_email(
+      recipient=user.email,
+      template_id=get_template_id(send_reset_link.__name__),
+      personalisation={
+        "user_name": user_name,
+        "reset_link": reset_link
+      }
+    )
+
+
+def send_invite(user,
+                group_dict=None,
+                role=None):
+    # type: (ckan.model.User,dict|None,str|None) -> None
+
+    if not user.email:
+      raise mailer.MailerException(_("No recipient email address available!"))
+
+    # use user ID, use user fullname if it is set
+    user_name = user.name
+    if user.fullname:
+      user_name = user.fullname
+
+    # generate a user reset key, then get it
+    mailer.create_reset_key(user)
+    reset_link = mailer.get_reset_link(user)
+
+    # get group type and name
+    group_type = "N/A"
+    group_title = "N/A"
+    if group_dict:
+      group_type = (_('organization') if group_dict['is_organization']
+                    else _('group'))
+      group_title = group_dict.get('title')
+
+    # get role name
+    role_name = "N/A"
+    if role:
+      role_name = roles_translated().get(role, _(role))
+    
+    send_email(
+      recipient=user.email,
+      template_id=get_template_id(send_invite.__name__),
+      personalisation={
+        "user_name": user_name,
+        "group_type": group_type,
+        "group_title": group_title,
+        "role_name": role_name,
+        "reset_link": reset_link
+      }
+    )
+
+
+def notify_ckan_user_create(email,
+                            fullname,
+                            username,
+                            phoneno,
+                            dept):
+  # type: (str, str, str, str, str) -> None
+
+  ###
+  # send email to canada.notification_new_user_email config if it exists
+  ###
+  if 'canada.notification_new_user_email' in config:
+
+    recipient_name = config.get(
+        'canada.notification_new_user_name',
+        config['canada.notification_new_user_email']
+    )
+
+    recipient_address = config['canada.notification_new_user_email']
+
+    send_email(
+      recipient=recipient_address,
+      template_id=get_template_id(notify_ckan_user_create.__name__),
+      personalisation={
+        "user_name": recipient_name
+      }
+    )
+
+  ###
+  # send email to form post values
+  ###
+  send_email(
+    recipient=email,
+    template_id=get_template_id(notify_ckan_user_create.__name__),
+    personalisation={
+      "user_name": fullname or email
+    }
+  )
+
+
+def get_request_headers(headers):
+    # type: (dict) -> dict|None
+
+    secret_key = config.get('ckanext.gcnotify.secret_key') # type: str
+
+    headers['Authorization'] = 'ApiKey-v1 {}'.format(secret_key)
+    headers['Content-Type'] = 'application/json'
+    headers['User-agent'] = 'CKAN/{}'.format(ckan_version())
+
+    return headers
+
+
+def get_api_endpoint(endpoint):
+    # type: (str) -> str|None
+
+    base_uri = config.get('ckanext.gcnotify.base_url') # type: str
+
+    if not endpoint:
+      raise mailer.MailerException(_("No GC Notify API endpoint is set!"))
+
+    return base_uri + endpoint
+
+
+def get_request_body(recipient,
+                    template_id,
+                    personalisation):
+    # type: (str, str, dict) -> dict|None
+
+    return {
+      'email_address': recipient,
+      'template_id': template_id,
+      'personalisation': personalisation
+    }
+
+
+def send_email(recipient,
+              template_id,
+              personalisation={},
+              headers={},
+              attachments=None):
+    # type: (str, str, dict, dict, dict|None) -> None
+
+    method = 'POST'
+    body_content = get_request_body(recipient,template_id,personalisation)
+    header_content = get_request_headers(headers)
+    endpoint = get_api_endpoint('/v2/notifications/email')
+
+    response = requests.request(
+      method=method,
+      url=endpoint,
+      json=body_content,
+      headers=header_content,
+      verify=False
+    )
+
+    response.raise_for_status()
--- ckanext/gcnotify/plugin.py
+++ ckanext/gcnotify/plugin.py
@@ -1,222 +1,23 @@
 import ckan.plugins as plugins
 
 import ckan.lib.mailer as mailer
-from ckan.common import _, config
-from ckan.lib.helpers import ckan_version
-from ckan.lib.helpers import roles_translated
-
-import requests
-import json
-import logging
-
-log = logging.getLogger(__name__) # type: logging.Logger
+import ckanext.gcnotify.mailer as mailer_overrider
+from ckan.common import config
 
 
 class GcnotifyPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+
+    assert config["ckanext.gcnotify.secret_key"]
+    assert config["ckanext.gcnotify.base_url"]
+    assert config["ckanext.gcnotify.template_ids"]
 
     # IConfigurer
 
     def update_config(self, config):
         # type: (object) -> None
 
-        mailer.send_reset_link = send_reset_link
-        mailer.send_invite = send_invite
-        mailer.notify_ckan_user_create = notify_ckan_user_create
+        mailer.send_reset_link = mailer_overrider.send_reset_link
+        mailer.send_invite = mailer_overrider.send_invite
+        mailer.notify_ckan_user_create = mailer_overrider.notify_ckan_user_create
 
-
-def get_template_id(action):
-    # type: (str) -> str|None
-
-    template_ids = config.get('ckanext.gcnotify.template_ids') # type: dict|None
-    if (template_ids is None) or not len(template_ids):
-      template_ids = {}
-    else:
-      template_ids = json.loads(template_ids)
-
-    if action not in template_ids:
-      raise mailer.MailerException(_("No GC Notify template ID is set!"))
-
-    return template_ids.get(action)
-
-
-def send_reset_link(user):
-    # type: (ckan.model.User) -> None
-
-    if (user.email is None) or not len(user.email):
-      raise mailer.MailerException(_("No recipient email address available!"))
-
-    # use user ID, use user fullname if it is set
-    user_name = user.name
-    if user.fullname is not None:
-      user_name = user.fullname
-
-    # generate a user reset key, then get it
-    mailer.create_reset_key(user)
-    reset_link = mailer.get_reset_link(user)
-    
-    send_email(
-      recipient=user.email,
-      template_id=get_template_id(send_reset_link.__name__),
-      personalisation={
-        "user_name": user_name,
-        "reset_link": reset_link
-      }
-    )
-
-
-def send_invite(user,
-                group_dict=None,
-                role=None):
-    # type: (ckan.model.User,dict|None,str|None) -> None
-
-    if (user.email is None) or not len(user.email):
-      raise mailer.MailerException(_("No recipient email address available!"))
-
-    # use user ID, use user fullname if it is set
-    user_name = user.name
-    if user.fullname is not None:
-      user_name = user.fullname
-
-    # generate a user reset key, then get it
-    mailer.create_reset_key(user)
-    reset_link = mailer.get_reset_link(user)
-
-    # get group type and name
-    group_type = "N/A"
-    group_title = "N/A"
-    if group_dict:
-      group_type = (_('organization') if group_dict['is_organization']
-                    else _('group'))
-      group_title = group_dict.get('title')
-
-    # get role name
-    role_name = "N/A"
-    if role:
-      role_name = roles_translated().get(role, _(role))
-    
-    send_email(
-      recipient=user.email,
-      template_id=get_template_id(send_invite.__name__),
-      personalisation={
-        "user_name": user_name,
-        "group_type": group_type,
-        "group_title": group_title,
-        "role_name": role_name,
-        "reset_link": reset_link
-      }
-    )
-
-
-def notify_ckan_user_create(email,
-                            fullname,
-                            username,
-                            phoneno,
-                            dept):
-  # type: (str, str, str, str, str) -> None
-
-  ###
-  # send email to canada.notification_new_user_email config if it exists
-  ###
-  if 'canada.notification_new_user_email' in config:
-
-    recipient_name = config.get(
-        'canada.notification_new_user_name',
-        config['canada.notification_new_user_email']
-    )
-
-    recipient_address = config['canada.notification_new_user_email']
-
-    send_email(
-      recipient=recipient_address,
-      template_id=get_template_id(notify_ckan_user_create.__name__),
-      personalisation={
-        "user_name": recipient_name
-      }
-    )
-
-  ###
-  # send email to form post values
-  ###
-  send_email(
-    recipient=email,
-    template_id=get_template_id(notify_ckan_user_create.__name__),
-    personalisation={
-      "user_name": fullname or email
-    }
-  )
-
-
-def get_request_headers(headers):
-    # type: (dict) -> dict|None
-
-    secret_key = config.get('ckanext.gcnotify.secret_key') # type: str
-
-    if (secret_key is None) or not len(secret_key):
-      raise mailer.MailerException(_("No GC Notify API key is set!"))
-
-    headers['Authorization'] = 'ApiKey-v1 {}'.format(secret_key)
-    headers['Content-Type'] = 'application/json'
-    headers['User-agent'] = 'CKAN/{}'.format(ckan_version())
-
-
-def get_api_endpoint(endpoint):
-    # type: (str) -> str|None
-
-    base_uri = config.get('ckanext.gcnotify.base_url') # type: str
-
-    if (base_uri is None) or not len(base_uri):
-      raise mailer.MailerException(_("No GC Notify base URI is set!"))
-
-    if (endpoint is None) or not len(endpoint):
-      raise mailer.MailerException(_("No GC Notify API endpoint is set!"))
-
-    return base_uri + endpoint
-
-
-def get_request_body(recipient,
-                    template_id,
-                    personalisation):
-    # type: (str, str, dict) -> dict|None
-
-    return {
-      'email_address': recipient,
-      'template_id': template_id,
-      'personalisation': personalisation
-    }
-
-
-def send_email(recipient,
-              template_id,
-              personalisation={},
-              headers={},
-              attachments=None):
-    # type: (str, str, dict, dict, dict|None) -> None
-
-    method = 'POST'
-    body_content = get_request_body(recipient,template_id,personalisation)
-    header_content = get_request_headers(headers)
-    endpoint = get_api_endpoint('/v2/notifications/email')
-
-    try:
-
-      response = requests.request(
-        method=method,
-        url=endpoint,
-        json=body_content,
-        headers=header_content,
-        verify=False
-      )
-
-      response.raise_for_status()
-
-    except requests.exceptions.HTTPError as error:
-
-      log.error("API {} request on {} failed with '{}'".format(
-                    method,
-                    endpoint,
-                    error))
-
-    finally:
-
-      log.debug("API {} request on {} finished".format(method, endpoint))
