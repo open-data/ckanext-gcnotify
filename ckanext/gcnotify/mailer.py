5b7943ad018b59d796818ba0622e2f9966246ec7
JVickery-TBS <jesse.vickery@tbs-sct.gc.ca>
2022-12-30 17:05:13 +0000

    Set SSL verify to false for now...

--- ckanext/gcnotify/mailer.py
+++ ckanext/gcnotify/mailer.py
@@ -186,7 +186,7 @@ def send_email(recipient,
       url=endpoint,
       json=body_content,
       headers=header_content,
-      verify=True
+      verify=False #TODO: set to True when SSL tunnel is available
     )
 
     response.raise_for_status()
