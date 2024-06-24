d5d08db69e2c9b1b69b23fa44a1b43b85377c9d0
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-06-24 19:16:14 +0000

    feat(dev): inotifier;
    - New `INotifier` implementation.

--- ckanext/gcnotify/plugin.py
+++ ckanext/gcnotify/plugin.py
@@ -4,23 +4,28 @@ from ckanext.gcnotify import mailer
 from ckan.common import config
 
 
+MAPPING = {
+    'request_password_reset': mailer.send_reset_link,
+    'user_invited': mailer.send_invite,
+    'user_created': mailer.notify_ckan_user_create,
+    'request_username_recovery': mailer.send_username_recovery,
+}
+
+
 class GcnotifyPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IMailer)
+    plugins.implements(plugins.INotifier)
 
     assert config["ckanext.gcnotify.secret_key"]
     assert config["ckanext.gcnotify.base_url"]
     assert config["ckanext.gcnotify.template_ids"]
 
-    # IMailer
+    # INotifier
 
-    def send_reset_link(self, user):
-        mailer.send_reset_link(user)
+    def notify_recipient(self, notification_sent,
+            recipient_name, recipient_email, subject,
+            body, body_html, headers, attachments):
+        return True
 
-    def send_invite(self, user, group_dict, role):
-        mailer.send_invite(user, group_dict, role)
-
-    def notify_ckan_user_create(self, email, fullname, username, phoneno, dept):
-        mailer.notify_ckan_user_create(email, fullname, username, phoneno, dept)
-
-    def send_username_recovery(self, email, username_list):
-        mailer.send_username_recovery(email, username_list)
+    def notify_about_topic(self, topic, details):
+        if topic in MAPPING:
+            MAPPING.get(topic)(**details)
